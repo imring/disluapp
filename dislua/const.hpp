3874d79da84ca41669921d74744cb1008b25d2af
imring <vitaliyvorobets25@gmail.com>
2020-09-19 19:38:39 +0300

    upload

---
+++ dislua/const.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+namespace dislua {
+    using byte  = unsigned char;
+    using word  = unsigned short;
+    using dword = unsigned long;
+
+    using uleb128 = dword;
+
+    /*!
+     * В данном перечислении представлен список версий компиляторов, поддерживаемых данной библиотекой.
+     * В случае, если используется неизвестный компилятор, используется COMPILER_UNKNOWN.		
+     */
+    enum COMPILERS {
+        COMPILER_UNKNOWN = -1,
+        COMPILER_LUAJIT2,
+    };
+};
